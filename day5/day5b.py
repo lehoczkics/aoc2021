ef5da6176c18de6f47db29d5c63dd5d7e8268120
Csaba Lehoczki <csaba@lehoczki.me>
2021-12-07 21:45:49 +0100

    day5 solved

---
+++ day5/day5b.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+
+def main():
+    with open("inputmod.txt") as f:
+            content = f.readlines();
+    
+    # sea is a 1000 x 1000 matrix of zeroes to store values
+    sea = np.zeros((1000,1000), dtype=int)
+
+    # read x1,y1, x2,y2
+    for line in content:
+        buff = line.split(",")
+        x1 = int(buff[0])
+        y1 = int(buff[1])
+        x2 = int(buff[2])
+        y2 = int(buff[3])
+
+        if x1 <= x2:
+            smallx = x1
+            bigx = x2
+        else :
+            smallx = x2
+            bigx = x1
+
+        if y1 <= y2:
+            smally = y1
+            bigy = y2
+        else :
+            smally = y2
+            bigy = y1
+
+    # the logic
+        if x1 == x2 : # vertical line
+            if y1 <= y2 :
+                for y in range(y1,y2+1):
+                    sea[x1,y] += 1
+            else :
+                for y in range(y2,y1+1):
+                    sea[x1,y] += 1
+
+        elif y1 == y2 : # horizontal line
+            if x1 <= x2 :
+                for x in range(x1,x2+1):
+                    sea[x,y1] += 1
+            else :
+                for x in range(x2,x1+1):
+                   sea[x,y1] += 1
+
+        else: # diagonal line
+            points = ((bigx - smallx)+1)
+            if (x2 - x1) * (y2 - y1) >= 0 : # upwards
+                for i in range(points):
+                    sea[(smallx+i),(smally+i)] +=1
+            else : # downwards
+                for j in range(points) :
+                    sea[(smallx+j),(bigy-j)] += 1
+
+    print(np.count_nonzero(sea >= 2))
+
+if __name__ == "__main__":
+    main()
